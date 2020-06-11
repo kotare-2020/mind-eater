b770889c93ebec76496c553bc0ac93a5d7fe3791
Joe Butler <joefish.b@gmail.com>
2020-06-12 11:10:01 +1200

    Fix broken stuff and add a css file

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,18 @@
 const express = require('express')
-
 const db = require('../db')
-
 const router = express.Router()
 
-router.get('/foodis/:id', (req, res)=>{
-
+router.get('/foodis/:id', (req, res) => {
   const id = req.params.id
-  
-  db.getRandomFoodId().then(id =>{
-    
- res.render('foodis', {id: id})
-  
+  db.getRandomFoodId().then(id => {
+    res.render('foodis', { id: id })
   })
+})
 
 router.get('/', (req, res) => {
   db.getPeopleData()
     .then(people => res.render('./pages/landing-page', people)
     )
 })
-  
+
 module.exports = router
