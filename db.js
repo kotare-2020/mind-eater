64ddaf2a26b65a3eec4b05982322e0bd907d67fc
student <student@eda-bc-01.localdomain>
2020-06-12 13:34:05 +1200

    add function to join tables, update route

--- db.js
+++ db.js
@@ -4,17 +4,36 @@ const connection = require('knex')(config)
 
 module.exports = {
   getRandomFoodId: getRandomFoodId,
-  getPeopleData: getPeopleData
+  getPeopleData: getPeopleData,
+  getPeopleAndFavFood:getPeopleAndFavFood
 }
 
 function getRandomFoodId (db = connection) {
  
-  const id = Math.floor(Math.random(99901, 99926))
   
-  return db('users').where('id', id)
+  min = Math.ceil(20001)
+  max = Math.floor(20050)
+
+  const foodId = Math.floor(Math.random( ) *(max-min +1) + min)
+  
+  // console.log(foodId);
+  
+   return db('food').where('id',foodId).first()
 
 }
 
+
 function getPeopleData ( db = connection ) {
   return db('people').select()
+}
+
+
+function getPeopleAndFavFood(id, db = connection) {
+  
+  
+  return db('people').join('food', 'people.food_id','food.id').select('people.name AS people_name', 'food.name AS food_name').where('people.id', id).first()
+  
+  //get people
+  //join to foods
+  //where people id = id
 }
--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,56 @@ const db = require('../db')
 const router = express.Router()
 
 router.get('/foodis/:id', (req, res) => {
+ 
   const id = req.params.id
-  db.getRandomFoodId().then(id => {
-    res.render('foodis', { id: id })
+  
+  let pageData = {}
+
+  db.getRandomFoodId()
+   
+  .then(data => {
+ 
+   pageData.randomFood = data.name
+  
+   
+   db.getPeopleAndFavFood(id).then(data =>{
+    
+    pageData.people_name = data.people_name
+    pageData.food_name = data.food_name
+    console.log(pageData);     
+    })
+  
+  }).then(
+
+    res.render('foodis', {pageData:pageData})
+  )
+  .catch((err)=>{
+
+    console.log(err);
+    
   })
-})
+   
+
+  })
+
+
+
+  
+  // .then(res.render('foodis',{ pagedata: pageData }))
+
+
+
+
+
+
+
 
 router.get('/', (req, res) => {
   db.getPeopleData()
     .then(people => res.render('./pages/landing-page', people)
     )
 })
+
+
 
 module.exports = router
