75f058f7ae53fa27ae07f742d00236736b0da010
student <student@eda-bc-01.localdomain>
2020-06-12 15:20:54 +1200

    deploy

--- db.js
+++ db.js
@@ -31,7 +31,10 @@ function getPeopleData ( db = connection ) {
 function getPeopleAndFavFood(id, db = connection) {
   
   console.log(Number(id), typeof id)
-  return db('people').join('food', 'people.food_id','food.id').where('people.id', Number(id)).select('people.name AS people_name', 'food.name AS food_name', 'people.id AS peopleid').first()
+  return db('people')
+    .join('food', 'people.food_id','food.id')
+    .where('people.id', Number(id))
+    .select('people.name AS people_name', 'food.name AS food_name', 'people.id AS peopleid').first()
   .then (data => {
     console.log(data)
     return data
--- migrations/20200611182551_people.js
+++ migrations/20200611182551_people.js
@@ -3,7 +3,7 @@ exports.up = (knex, Promise) => {
     return knex.schema.createTable('people', (table) => {
       table.increments('id').primary()
       table.string('name')
-      table.string('food_id')
+      table.integer('food_id')
     })
   }
   
